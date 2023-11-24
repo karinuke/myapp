ec5536b9c08d9d59016b915ec8f21358e2ca5e29
karin <karin.arata1010@gmail.com>
2023-11-24 08:06:25 +0000

    11/24

--- resources/views/recipe/post.blade.php
+++ resources/views/recipe/post.blade.php
@@ -5,13 +5,39 @@
 @section('content')
 
 @foreach ($posts as $post)
- <div class="circle_post">
-    <div class="circle_postimage">
-            <img src="{{ secure_asset('storage/image/'.$post->image_path) }}">
-    </div>
+<div class='post'>
+  <div class="circle_post">
+     <div class="circle_postimage">
+             <img src="{{ secure_asset('storage/image/'.$post->image_path) }}">
+     </div>
+  </div>
+  
+  <div="recipe_posts"> 
+   <p>{{$post->genre}}</p>
+   
+   <h2>
+       <p>{{$post->title}}</p>
+   </h2>
+   <h3>
+   <p>{{$post->detail}}</p>
+   </h3>
+   材料
+   <div class="box_materials">
+   <p>{{$post->materials}}</p>
+   </div>
+    
+    <br>
+  
+      作り方
+      <div class="box_recipe">
+      <p>{{$post->recipe}}</p>
+      </div>
+  </div>
  </div>
 
 @endforeach
+
+
 
 @endsection
 
